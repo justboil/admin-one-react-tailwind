4a63b8819abaebd6e8d8912c984c9152c4c06e7d
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-09-14 17:12:46 +0500

    add: responsive page

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@
  */
  const nextConfig = {
   basePath: "/admin-one-react-tailwind",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.justboil.me',
+      },
+    ],
+  },
 }
 
 export default nextConfig
