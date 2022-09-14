d80bcdce42a042ed1263b4351022b0d3edb5bfd5
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-09-14 16:27:12 +0500

    add: tables page & empty card component

---
+++ components/CardBoxComponentEmpty.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardBoxComponentEmpty = () => {
+  return (
+    <div className="text-center py-24 text-gray-500 dark:text-slate-400">
+      <p>Nothing's here…</p>
+    </div>
+  )
+}
+
+export default CardBoxComponentEmpty;
---
+++ pages/tables.tsx
@@ -0,0 +1,53 @@
+import { mdiGithub, mdiMonitorCellphone, mdiTableBorder, mdiTableOff } from "@mdi/js";
+import React, { ReactElement } from "react";
+import BaseButton from "../components/BaseButton";
+import CardBox from "../components/CardBox";
+import CardBoxComponentEmpty from "../components/CardBoxComponentEmpty";
+import LayoutAuthenticated from "../components/layouts/Authenticated";
+import NotificationBar from "../components/NotificationBar";
+import SectionMain from "../components/SectionMain";
+import SectionTitleLineWithButton from "../components/SectionTitleLineWithButton";
+import TableSampleClients from "../components/TableSampleClients";
+
+const TablesPage = () => {
+  return (
+    <SectionMain>
+      <SectionTitleLineWithButton icon={mdiTableBorder} title="Tables" main>
+        <BaseButton
+          href="https://github.com/justboil/admin-one-vue-tailwind"
+          target="_blank"
+          icon={mdiGithub}
+          label="Star on GitHub"
+          color="contrast"
+          roundedFull
+          small
+        />
+      </SectionTitleLineWithButton>
+
+      <NotificationBar color="info" icon={mdiMonitorCellphone}>
+        <b>Responsive table.</b> Collapses on mobile
+      </NotificationBar>
+
+      <CardBox className="mb-6" has-table>
+        <TableSampleClients />
+      </CardBox>
+
+      <SectionTitleLineWithButton icon={mdiTableOff} title="Empty variation" />
+
+      <NotificationBar color="danger" icon={mdiTableOff}>
+        <b>Empty card.</b> When there's nothing to show
+      </NotificationBar>
+
+      <CardBox>
+        <CardBoxComponentEmpty />
+      </CardBox>
+
+    </SectionMain>
+  )
+}
+
+TablesPage.getLayout = function getLayout(page: ReactElement) {
+  return <LayoutAuthenticated>{page}</LayoutAuthenticated>
+}
+
+export default TablesPage;
