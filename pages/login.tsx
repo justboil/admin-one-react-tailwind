61b5d088fbb2c2f05ca516eeff748afc0b949072
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-09-17 00:00:16 +0500

    add: login demo

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,25 +1,64 @@
+import React from 'react'
+import type { ReactElement } from 'react'
 import Head from 'next/head'
-import { ReactElement } from 'react'
-import LayoutAuthenticated from '../components/layouts/Authenticated'
-import SectionTitle from '../components/SectionTitle'
+import BaseButton from '../components/BaseButton'
+import CardBox from '../components/CardBox'
+import SectionFullScreen from '../components/SectionFullScreen'
+import LayoutGuest from '../components/layouts/Guest'
 import { appTitle } from '../src/config'
+import { Field, Form, Formik } from 'formik'
+import FormField from '../components/FormField'
+import FormCheckRadio from '../components/FormCheckRadio'
+import BaseDivider from '../components/BaseDivider'
+import BaseButtons from '../components/BaseButtons'
+import { useRouter } from 'next/router'
 
-const LoginPage = () => {
+export default function Error() {
+  const router = useRouter()
+
+  const handleSubmit = () => {
+    router.push('/dashboard')
+  }
+
   return (
     <>
       <Head>
         <title>Login - {appTitle}</title>
       </Head>
 
-      <div className="min-h-screen">
-        <SectionTitle first>Demo is coming soon</SectionTitle>
-      </div>
+      <SectionFullScreen bg="purplePink">
+        <CardBox className="w-11/12 md:w-7/12 lg:w-6/12 xl:w-4/12 shadow-2xl">
+          <Formik
+            initialValues={{ login: 'john.doe', password: 'bG1sL9eQ1uD2sK3b', remember: true }}
+            onSubmit={() => handleSubmit()}
+          >
+            <Form>
+              <FormField label="Login" help="Please enter your login">
+                <Field name="login" />
+              </FormField>
+
+              <FormField label="Password" help="Please enter your password">
+                <Field name="password" type="password" />
+              </FormField>
+
+              <FormCheckRadio type="checkbox" label="Remember">
+                <Field type="checkbox" name="remember" />
+              </FormCheckRadio>
+
+              <BaseDivider />
+
+              <BaseButtons>
+                <BaseButton type="submit" label="Login" color="info" />
+                <BaseButton href="/dashboard" label="Home" color="info" outline />
+              </BaseButtons>
+            </Form>
+          </Formik>
+        </CardBox>
+      </SectionFullScreen>
     </>
   )
 }
 
-LoginPage.getLayout = function getLayout(page: ReactElement) {
-  return <LayoutAuthenticated>{page}</LayoutAuthenticated>
+Error.getLayout = function getLayout(page: ReactElement) {
+  return <LayoutGuest>{page}</LayoutGuest>
 }
-
-export default LoginPage
