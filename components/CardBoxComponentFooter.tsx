3db35186bf140a779477ab997e898483c0f49e0a
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-11-25 19:51:06 +0500

    add: className

--- components/CardBoxComponentFooter.tsx
+++ components/CardBoxComponentFooter.tsx
@@ -1,9 +1,10 @@
 import React, { ReactNode } from 'react'
 
 type Props = {
+  className?: string
   children?: ReactNode
 }
 
-export default function CardBoxComponentFooter({ children }: Props) {
-  return <footer className="p-6">{children}</footer>
+export default function CardBoxComponentFooter({ className, children }: Props) {
+  return <footer className={`p-6 ${className}`}>{children}</footer>
 }
