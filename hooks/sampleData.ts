8aff286ef3f3dd1b31fc19734756b7131df4839c
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-11-25 18:29:22 +0500

    rem: unused import

--- hooks/sampleData.ts
+++ hooks/sampleData.ts
@@ -1,6 +1,4 @@
 import useSWR from 'swr'
-import { Transaction } from '../interfaces'
-
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export const useSampleClients = () => {
