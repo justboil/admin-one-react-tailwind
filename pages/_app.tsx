f30e8585db8d7ead1fad549d6240a319a2958474
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-11-30 14:28:03 +0500

    add: og meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,13 +19,45 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
+  const title = `Admin One React Tailwind free`
+
+  const description =
+    'Admin One - free React Next Tailwind dashboard with TypeScript and dark mode'
+
+  const url = 'https://justboil.github.io/admin-one-react-tailwind/'
+
+  const image = `https://static.justboil.me/templates/one/repo-tailwind-react.png`
+
+  const imageWidth = '1920'
+
+  const imageHeight = '960'
+
   return (
     <Provider store={store}>
       {getLayout(
         <>
           <Head>
+          <meta name="description" content={description} />
+
+          <meta property="og:url" content={url} />
+          <meta property="og:site_name" content="JustBoil.me" />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={description} />
+          <meta property="og:image" content={image} />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content={imageWidth} />
+          <meta property="og:image:height" content={imageHeight} />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:title" content={title} />
+          <meta property="twitter:description" content={description} />
+          <meta property="twitter:image:src" content={image} />
+          <meta property="twitter:image:width" content={imageWidth} />
+          <meta property="twitter:image:height" content={imageHeight} />
+
             <link rel="icon" href="/admin-one-react-tailwind/favicon.png" />
           </Head>
+          
           <Component {...pageProps} />
         </>
       )}
