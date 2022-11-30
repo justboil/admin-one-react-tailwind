5940f828aa7a51fa19d1c4491fa94d703c5758c2
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-11-30 17:36:13 +0500

    add: profile demo

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,19 +1,158 @@
+import { mdiAccount, mdiAsterisk, mdiFormTextboxPassword, mdiGithub, mdiMail } from '@mdi/js'
+import { Formik, Form, Field } from 'formik'
 import Head from 'next/head'
 import { ReactElement } from 'react'
+import BaseButton from '../components/BaseButton'
+import BaseButtons from '../components/BaseButtons'
+import BaseDivider from '../components/BaseDivider'
+import CardBox from '../components/CardBox'
+import CardBoxComponentBody from '../components/CardBoxComponentBody'
+import CardBoxComponentFooter from '../components/CardBoxComponentFooter'
+import FormField from '../components/FormField'
 import LayoutAuthenticated from '../components/layouts/Authenticated'
-import SectionTitle from '../components/SectionTitle'
+import SectionMain from '../components/SectionMain'
+import SectionTitleLineWithButton from '../components/SectionTitleLineWithButton'
+import UserCard from '../components/UserCard'
 import { getPageTitle } from '../src/app'
+import { useAppSelector } from '../src/stores/hooks'
+
+type UserForm = {
+  name: string
+  email: string
+}
 
 const ProfilePage = () => {
+  const userName = useAppSelector((state) => state.main.userName)
+  const userEmail = useAppSelector((state) => state.main.userEmail)
+
+  const userForm: UserForm = {
+    name: userName,
+    email: userEmail,
+  }
+
   return (
     <>
       <Head>
         <title>{getPageTitle('Profile')}</title>
       </Head>
 
-      <div className="min-h-screen">
-        <SectionTitle first>Demo is coming soon</SectionTitle>
-      </div>
+      <SectionMain>
+        <SectionTitleLineWithButton icon={mdiAccount} title="Profile" main>
+          <BaseButton
+            href="https://github.com/justboil/admin-one-react-tailwind"
+            target="_blank"
+            icon={mdiGithub}
+            label="Star on GitHub"
+            color="contrast"
+            roundedFull
+            small
+          />
+        </SectionTitleLineWithButton>
+
+        <UserCard className="mb-6" />
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <CardBox hasComponentLayout>
+            <Formik
+              initialValues={userForm}
+              onSubmit={(values: UserForm) => alert(JSON.stringify(values, null, 2))}
+            >
+              <Form className="flex flex-col flex-1">
+                <CardBoxComponentBody>
+                  <FormField
+                    label="Name"
+                    help="Required. Your name"
+                    labelFor="name"
+                    icons={[mdiAccount]}
+                  >
+                    <Field name="name" id="name" placeholder="Name" />
+                  </FormField>
+                  <FormField
+                    label="E-mail"
+                    help="Required. Your e-mail"
+                    labelFor="email"
+                    icons={[mdiMail]}
+                  >
+                    <Field name="email" id="email" placeholder="E-mail" />
+                  </FormField>
+                </CardBoxComponentBody>
+                <CardBoxComponentFooter>
+                  <BaseButtons>
+                    <BaseButton color="info" type="submit" label="Submit" />
+                    <BaseButton color="info" label="Options" outline />
+                  </BaseButtons>
+                </CardBoxComponentFooter>
+              </Form>
+            </Formik>
+          </CardBox>
+
+          <CardBox hasComponentLayout>
+            <Formik
+              initialValues={{
+                currentPassword: '',
+                newPassword: '',
+                newPasswordConfirmation: '',
+              }}
+              onSubmit={(values) => alert(JSON.stringify(values, null, 2))}
+            >
+              <Form className="flex flex-col flex-1">
+                <CardBoxComponentBody>
+                  <FormField
+                    label="Current password"
+                    help="Required. Your current password"
+                    labelFor="currentPassword"
+                    icons={[mdiAsterisk]}
+                  >
+                    <Field
+                      name="currentPassword"
+                      id="currentPassword"
+                      type="password"
+                      autocomplete="current-password"
+                    />
+                  </FormField>
+
+                  <BaseDivider />
+
+                  <FormField
+                    label="New password"
+                    help="Required. New password"
+                    labelFor="newPassword"
+                    icons={[mdiFormTextboxPassword]}
+                  >
+                    <Field
+                      name="newPassword"
+                      id="newPassword"
+                      type="password"
+                      autocomplete="new-password"
+                    />
+                  </FormField>
+
+                  <FormField
+                    label="Confirm password"
+                    help="Required. New password one more time"
+                    labelFor="newPasswordConfirmation"
+                    icons={[mdiFormTextboxPassword]}
+                  >
+                    <Field
+                      name="newPasswordConfirmation"
+                      id="newPasswordConfirmation"
+                      type="password"
+                      autocomplete="new-password"
+                    />
+                  </FormField>
+                </CardBoxComponentBody>
+
+                <CardBoxComponentFooter>
+                  <BaseButtons>
+                    <BaseButton color="info" type="submit" label="Submit" />
+                    <BaseButton color="info" label="Options" outline />
+                  </BaseButtons>
+                </CardBoxComponentFooter>
+              </Form>
+            </Formik>
+          </CardBox>
+        </div>
+      </SectionMain>
     </>
   )
 }
