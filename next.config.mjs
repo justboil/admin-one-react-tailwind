475870a4175cb451d906db4f3e2e141d8cbf589c
Viktor Kuzhelnyi <viktor@marketto.ru>
2022-12-05 22:31:30 +0500

    add: basePath redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@
  */
  const nextConfig = {
   basePath: "/admin-one-react-tailwind",
+  async redirects() {
+    return [
+      {
+          source: '/',
+          destination: '/admin-one-react-tailwind',
+          basePath: false,
+          permanent: false
+      }
+    ]
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
