56b2a1caa3f3d9ddbd0731b58e5f60dc629fc36b
vikdiesel <viktor@marketto.ru>
2025-02-24 18:26:19 +0500

    add: eslint-disable to taiwind.config.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,7 @@ module.exports = {
     },
   },
   plugins: [
+    /* eslint-disable-next-line */
     require("@tailwindcss/forms"),
     plugin(function ({ matchUtilities, theme }) {
       matchUtilities(
